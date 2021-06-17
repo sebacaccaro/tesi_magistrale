068ca10308e27ac1c83a4ddad773ef23b11b06f0
sebacaccaro <sebastianocaccaro@gmail.com>
2021-06-17 16:16:13 +0200

    bugfix

--- Creazione_Dataset/dataset_perturbation_generator.py
+++ Creazione_Dataset/dataset_perturbation_generator.py
@@ -20,13 +20,15 @@ print("Caricando le frasi estratte...")
 with open("extracted.json")as f:
     dataset = json.load(f)
 
-dataset = [{**d, "perturbed": {}} for d in dataset]
+dataset = [{**d, "perturbed": {}} for d in dataset][:10]
+
+sup_pipelines = {"M2": sup_pipelines["M2"]}
 
 i = 0
 for sup_name, sup in sup_pipelines.items():
     desc = f"Perturbando con {sup_name} ({i}/{len(sup_pipelines)})"
     i += 1
-    dataset = [perturbed_sample(s, sup.run(s), sup_name)
+    dataset = [perturbed_sample(s, sup.run(s["text"]), sup_name)
                for s in tqdm(dataset, desc=desc)]
 
 with open("dataset.json", "w") as f:
