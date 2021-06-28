99446624433085af1aec7358d2a19ee6487877a9
sebacaccaro <sebastianocaccaro@gmail.com>
2021-06-29 00:39:13 +0200

    Valutazione frasi

--- Valutazione/valutazione.py
+++ Valutazione/valutazione.py
@@ -1,74 +1,165 @@
 import os
+from tqdm import tqdm
 import json
+from Levenshtein import matching_blocks, editops
 
-eval_folder = "./dataset_valutati"
-out_file = "valutazione.json"
-
-correction_folders = {
-    "project_gender_politics": "../Metodi_correzione/project_gender_politics/corrections/"
-}
-
-blackList = [
-    "errors_",
-    ".md"
-]
+corrections_folders = {
+    "project_gender_politcs": "../Metodi_correzione/project_gender_politics/corrections/"}
 
 
-def blackListed(filename):
-    return any([x in filename for x in blackList])
+def diffAlign(s1, s2):
+    matching = matching_blocks(
+        editops(s1, s2), s1, s2)
+    s1 = [c for c in s1]
+    s2 = [c for c in s2]
+    s1n = []
+    s2n = []
+    popped1, added1 = 0, 0
+    popped2, added2 = 0, 0
+    for mb in matching:
+        index1 = mb[0]
+        index2 = mb[1]
+        # Controllo allineamento indici
+        while(popped1 < index1):
+            s1n.append(s1.pop(0))
+            popped1 += 1
+        while(popped2 < index2):
+            s2n.append(s2.pop(0))
+            popped2 += 1
+        while(len(s1n) < len(s2n)):
+            s1n.append("$")
+            added1 += 1
+        while(len(s2n) < len(s1n)):
+            s2n.append("$")
+            added2 += 1
+
+        seqLen = mb[2]
+        for i in range(seqLen):
+            s1n.append(s1.pop(0))
+            s2n.append(s2.pop(0))
+            popped1, popped2 = popped1 + 1, popped2 + 1
+
+    s1n = "".join(s1n)
+    s2n = "".join(s2n)
+    return s1n, s2n
 
 
-def corrected_errors(datapoint):
-    return 0
+def align(datapoint):
+    s1, s2 = diffAlign(datapoint["text"], str.strip(datapoint["perturbed"]))
+    s1f, s2f = s1, s2
+    s2, s3 = diffAlign(s2, str.strip(datapoint["corrected"]))
+    s1, s3 = diffAlign(s1, s3)
+    if len(s1) == len(s2) == len(s3):
+        return s1, s2, s3, True
+    else:
+        return s1f, s2f, s3, False
 
 
-def inserted_errors(datapoint):
-    return 0
+def errorBitMask(c, cp, cpp):
+    return [0 if c[i] == cp[i] == cpp[i] else 1 for i in range(len(c))]
 
 
-def original_errors(datapoint):
-    return 0
+def bitMaskToPos(bitmask):
+    ranges = []
+    current = []
+    for i in range(len(bitmask)):
+        value = bitmask[i]
+        if value == 0:
+            if len(current) > 0:
+                ranges.append(current)
+                current = []
+        elif value == 1:
+            current.append(i)
+    if len(current) > 0:
+        ranges.append(current)
+    return ranges
 
 
-def evalCorrection(correction_set):
-    def prefix_str(
-        x): return f"({correction_set['project']}-{correction_set['pert_level']}) {x}"
+def errorChars(sentence, ranges):
+    return ["".join([sentence[i] for i in r]) for r in ranges]
 
-    print(prefix_str("Leggendo il dataset..."))
-    with open(correction_set["path"]) as f:
-        dataset = json.load(f)
-    print(prefix_str("Calcolando gli errori..."))
-    for datapoint in dataset:
-        datapoint["original_errors"] = original_errors(datapoint)
-        datapoint["corrected_errors"] = corrected_errors(datapoint)
-        datapoint["inserted_errors"] = inserted_errors(datapoint)
 
-    print(prefix_str("Salvando il dataset con le correzioni..."))
-    with open(f"{eval_folder}/{correction_set['project']}_{correction_set['pert_level']}.json", "w") as f:
-        json.dump(dataset, f, indent=2)
-
+def standardScore(c, cp, cpp):
+    errors = bitMaskToPos(errorBitMask(c, cp, cpp))
+    c = errorChars(c, errors)
+    cp = errorChars(cp, errors)
+    cpp = errorChars(cpp, errors)
+    perturbation_errors = sum([1 for i in range(len(c)) if c[i] != cp[i]])
+    corrected_errors = sum(
+        [1 for i in range(len(c)) if c[i] != cp[i] and cpp[i] == c[i]])
+    introduced_errors = sum(
+        [1 for i in range(len(c)) if c[i] == cp[i] and cpp[i] != c[i]])
     return {
-        **correction_set,
-        "errors_stats": {
-            "original_errors": sum([d["original_errors"] for d in dataset]),
-            "corrected_errors": sum([d["corrected_errors"] for d in dataset]),
-            "inserted_errors": sum([d["inserted_errors"] for d in dataset]),
-        }
+        "perturbation_errors": perturbation_errors,
+        "corrected_errors": corrected_errors,
+        "introduced_errors": introduced_errors,
     }
 
 
-folders_files = {project:
-                 [{"path": folder + file, "pert_level": file.replace(".json", ""), "project": project}
-                     for file in os.listdir(folder) if not blackListed(file)]
-                 for project, folder in correction_folders.items()}
+def errorScore(c, cp, cpp):
+    errors = bitMaskToPos(errorBitMask(c, cp, cp))
+    c = errorChars(c, errors)
+    cp = errorChars(cp, errors)
+    perturbation_errors = sum([1 for i in range(len(c)) if c[i] != cp[i]])
+    corrected_errors = 0
+    introduced_errors = perturbation_errors
+    return {
+        "perturbation_errors": perturbation_errors,
+        "corrected_errors": corrected_errors,
+        "introduced_errors": introduced_errors,
+    }
 
 
-evaluations = {
-    project: [evalCorrection(correction) for correction in corrections]
-    for project, corrections in folders_files.items()
-}
+def correctionScore(sample):
+    c = sample["orginal"]
+    cp = sample["perturbed"]
+    cpp = sample["corrected"]
+    return standardScore(c, cp, cpp) if sample["alignedSuccess"] else errorScore(c, cp, cpp)
 
-with open(out_file, "w") as f:
-    json.dump(evaluations, f, indent=2)
 
-print(f"I risultati sono stati valutati, output scritto in {out_file}")
+def dataset_stats(dataset):
+    dataset = [align(datapoint)
+               for datapoint in tqdm(dataset, desc="    > Allineando Frasi")]
+    dataset = [{
+        "orginal": x[0],
+        "perturbed": x[1],
+        "corrected": x[2],
+        "alignedSuccess": x[3]
+    } for x in dataset]
+    dataset = [correctionScore(sample) for sample in tqdm(
+        dataset, desc=" > Valutando Correzioni")]
+    stats = {
+        "perturbation_errors": sum([x["perturbation_errors"] for x in dataset]),
+        "corrected_errors": sum([x["corrected_errors"] for x in dataset]),
+        "introduced_errors": sum([x["introduced_errors"] for x in dataset]),
+        "total_samples": len(dataset)
+    }
+    return stats
+
+
+def fileNameToStats(filename, correction_folder):
+    print(f"    > Valutando {dataset_name(filename)}...")
+    with open(correction_folder + filename) as f:
+        corrections = json.load(f)
+    return dataset_stats(corrections)
+
+
+def dataset_name(filename):
+    return filename.replace(".json", "")
+
+
+def evaluate_project(project_foler, corr_name):
+    print(f"Valutando {corr_name}...")
+    files = os.listdir(project_foler)
+    files = [f for f in files if ".json" in f and "errors_" not in f]
+    stats = {dataset_name(f): fileNameToStats(f, project_foler) for f in files}
+    return stats
+
+
+stats = {corr_name: evaluate_project(
+    corr_folder, corr_name) for corr_name, corr_folder in tqdm(corrections_folders.items())}
+
+with open("valutazione.json", "w")as f:
+    json.dump(stats, f, indent=2)
+
+print("Fatto! Output scritto in valutaizone.json")
