87447a7a3065dde884eb454b521cbbfe1e1a14d5
sebacaccaro <sebastianocaccaro@gmail.com>
2021-07-10 11:30:11 +0200

    Fix reduced script

--- Creazione_Dataset/create_reduced.py
+++ Creazione_Dataset/create_reduced.py
@@ -1,12 +1,15 @@
 import json
 import random
 
-with open("dataset_v2f.json") as f:
+print("Leggendo dataset")
+with open("dataset.json") as f:
     dataset = json.load(f)
 
+print("Mescolando dataset")
 random.shuffle(dataset)
 
 dataset = dataset[:10000]
 
-with open("dataset_v2f_reduced.json", "w") as f:
+print("Scrivendo versione ridotta")
+with open("dataset_reduced.json", "w") as f:
     json.dump(dataset, f, indent=2)
