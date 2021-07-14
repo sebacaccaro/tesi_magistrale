c761a957720a47bcc02801712f2e488e4cda090c
sebacaccaro <sebastianocaccaro@gmail.com>
2021-07-14 02:14:14 +0200

    update formatter

--- Metodi_correzione/openNMT/dataset_formatter.py
+++ Metodi_correzione/openNMT/dataset_formatter.py
@@ -1,7 +1,18 @@
 import json
+from os import write
+from random import shuffle
 
+validation_size = 5000
+
+print("Leggendo il dataset")
 with open('../../Creazione_Dataset/dataset_reduced.json') as data_file:
     dataset = json.load(data_file)
+
+print("Mescolando il dataset")
+shuffle(dataset)
+
+validation = dataset[:validation_size]
+dataset = dataset[validation_size:]
 
 
 #function that takes a string a sub the spaces with #
@@ -11,22 +22,31 @@ def sub_spaces(string):
     return string
 
 
-keys = list(dataset[0]["perturbed"].keys())
+# Write the dataset to files
+def write_files(dataset, folderName):
+    keys = list(dataset[0]["perturbed"].keys())
+    for key in keys:
+        sentenceList = []
+        for datapoint in dataset:
+            formatted_sentence = sub_spaces(datapoint["perturbed"][key])
+            sentenceList.append(formatted_sentence)
+        with open(f"./{folderName}/{key}.tgt", "w") as f:
+            for sentence in sentenceList:
+                f.write(sentence + "\n")
 
-for key in keys:
+
+def write_files_original(dataset, folderName):
     sentenceList = []
     for datapoint in dataset:
-        formatted_sentence = sub_spaces(datapoint["perturbed"][key])
-        sentenceList.append(formatted_sentence)
-    with open(f"./targets/{key}.tgt", "w") as f:
+        sentence = sub_spaces(datapoint["text"])
+        sentenceList.append(sentence)
+
+    with open(f"./{folderName}/original.src", "w") as f:
         for sentence in sentenceList:
             f.write(sentence + "\n")
 
-sentenceList = []
-for datapoint in dataset:
-    sentence = sub_spaces(datapoint["text"])
-    sentenceList.append(sentence)
 
-with open("./original.src", "w") as f:
-    for sentence in sentenceList:
-        f.write(sentence + "\n")
+write_files_original(dataset, "train")
+write_files_original(validation, "test")
+write_files(dataset, "train")
+write_files(validation, "test")
