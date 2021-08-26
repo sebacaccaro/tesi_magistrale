595ea166843718ea75aba465c3f272f725c2c72c
sebacaccaro <sebastianocaccaro@gmail.com>
2021-08-26 15:59:43 +0200

    tester_correzione

--- Metodi_correzione/bert/main.py
+++ Metodi_correzione/bert/main.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 from bert_filler import Filler
 from corrector import Corrector
+import json
+from tqdm import tqdm
 
 filler = Filler()
 corrector = Corrector("lexicon.txt", filler)
 
 
-res = corrector.correct("La cssa era seiiza un tetto.")
-print(res)
+with open("../../Creazione_Dataset/dataset_v2f_reduced.json") as f:
+    dataset = json.load(f)
+
+dataset = [{
+    "text": datapoint["text"],
+    "perturbed": datapoint["perturbed"]["T3"],
+    "corrected": corrector.correct(datapoint["perturbed"]["T3"])
+} for datapoint in tqdm(dataset)
+]
+
+with open("correction_output.json", "w") as f:
+    json.dump(dataset, f, indent=2)
