5688174d57b4c23b96b62c4b95a8f25f46e1e3e9
sebacaccaro <sebastianocaccaro@gmail.com>
2021-08-27 14:59:05 +0200

    v2 correzione

--- Metodi_correzione/bert/corrector.py
+++ Metodi_correzione/bert/corrector.py
@@ -1,18 +1,35 @@
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from bert_filler import Filler
 from nltk import word_tokenize
+import string
+import re
+from Levenshtein import distance
 
 
 def detokenize(input: list):
     output = TreebankWordDetokenizer().detokenize(input)
     output = output.replace(" , ", ", ")
     output = output.replace(" ' ", "'")
+    output = output.replace("' ", "'")
     output = output.replace(" ’ ", "’")
     output = output.replace(" ’", "’")
     output = output.replace(" . ", ". ")
     output = output.replace(" : ", ": ")
     output = output.replace(" ; ", "; ")
     return output
+
+
+def mod_tokenize(sentence):
+    tokens = word_tokenize(sentence)
+    modTokens = []
+    for token in tokens:
+        if "'" in token:
+            divided = [str.strip(t)
+                       for t in re.split("(')",  token) if t != '']
+            modTokens.extend(divided)
+        else:
+            modTokens.append(token)
+    return modTokens
 
 
 class Corrector:
@@ -24,21 +41,47 @@ class Corrector:
         with open(dict_path, 'r', encoding='utf-8') as f:
             for line in f:
                 self.vocabulary.add(line.strip())
+        for punct in string.punctuation:
+            self.vocabulary.add(punct)
 
     def enable_log(self) -> None:
         self.log = True
 
-    def isError(self, word: str) -> bool:
-        return word.lower() not in self.vocabulary
+    def isError(self, words: str, index: int) -> bool:
+        if len(words[index]) <= 1:
+            return False
+        if words[index].lower() not in self.vocabulary:
+            # Checking if error word is a word with apostrophe
+            if index + 1 < len(words) and words[index+1] == "'":
+                word = words[index]
+                possibleFull = [
+                    word.lower() + c for c in ["a", "e", "i", "o", "u"]]
+                return not any([p in self.vocabulary for p in possibleFull])
+            return True
+        return False
+
+    def isCorrectionCloseEnough(original, correction):
+        editDistance = distance(original, correction)
+        if len(original) > 10:
+            if editDistance < 5:
+                return True
+        elif len(original) > 5:
+            if editDistance < 4:
+                return True
+        else:
+            if editDistance < 3:
+                return True
+        return False
 
     def correct(self, sentence):
-        tokens = word_tokenize(sentence)
+        tokens = mod_tokenize(sentence)
         for i in range(len(tokens)):
             token = tokens[i]
-            if self.isError(token):
+            if self.isError(tokens, i):
                 sentence = detokenize(
                     [*tokens[:i], self.filler.FILL_STR, *tokens[i+1:]])
                 correction = self.filler.guess(sentence, token)
-                tokens[i] = correction
+                if (self.isCorrectionCloseEnough(tokens[i], correction)):
+                    tokens[i] = correction
         sentence = detokenize(tokens)
         return sentence
