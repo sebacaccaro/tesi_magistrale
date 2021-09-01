97468f233e7e1cd58fef08636322c9c53e9d742a
sebacaccaro <sebastianocaccaro@gmail.com>
2021-09-01 10:59:42 +0200

    corrector v4

--- Metodi_correzione/bert/splitcorrector.py
+++ Metodi_correzione/bert/splitcorrector.py
@@ -1,7 +1,10 @@
 import json
 import re
 from typing import Match
-from utils import cleanOutput
+from utils import cleanOutput, string_subtract
+from bert_filler import Filler
+from Levenshtein import distance
+
 
 examples = ["millenario della s u a nascita,, che si celebra in",
             "d i l e t t i membri, d e l l a vostra Associazione,. che",
@@ -20,9 +23,11 @@ class SplitCorrector:
 
     # Regex per trovare le parole frammentate
     split_reg = r'(?:\s|^)(\w(?:\W?\s\w{1,2}){3,})(?:\W|\s|$)'
+    max_accettable_distance = 4
 
-    def __init__(self, vocabulary: set) -> None:
+    def __init__(self, vocabulary: set, filler: Filler) -> None:
         self.vocabulary = vocabulary
+        self.filler = filler
 
     def __isMatchCorrect(self, matchedString):
         """ 
@@ -43,15 +48,14 @@ class SplitCorrector:
         return sentence
 
     def __fix_with_vocab(self, sentence: str, match: Match, offset: int):
-        segment = match.group(1)
-        segment = segment.replace(" ", "")
+        segment = match.group(1).replace(" ", "")
         if segment in self.vocabulary:
             return self.__replace_match(sentence, match, segment, offset)
         return None
 
-    def __fix_with_filler(self):
+    def __fix_with_filler(self, sentence, match, offset):
         """ 
-        Ci sono vari casi a considerare:
+        Ci sono vari casi da considerare:
         - La parola viene identificata e corretta perchè è semplicemente una parola spezzettata
             Ex. d i l e t t i membri, d e l l a vostra Associazione
         - La regex comprende due parole. Bert potrebbe indovinarne una. Si potrebbe pensare di fare
@@ -59,10 +63,21 @@ class SplitCorrector:
             Ex. coinvolsero ogni uomo e o g n i donna, procurando.
         - No clue > Sad
         """
-        pass
+        segment = match.group(1).replace(" ", "")
+        maskedSentence = self.__replace_match(
+            sentence, match, self.filler.FILL_STR, offset)
+        guessed_word = self.filler.guess(maskedSentence, segment)
+
+        left_word, right_word = string_subtract(segment, guessed_word)
+        if left_word:
+            return self.__replace_match(sentence, match, left_word + " " + right_word, offset)
+
+        if distance(guessed_word, segment) < self.max_accettable_distance:
+            return self.__replace_match(sentence, match, guessed_word, offset)
+        return None
 
     def __fix_match(self, sentence, match, offset):
-        return self.__fix_with_vocab(sentence, match, offset) or sentence
+        return self.__fix_with_vocab(sentence, match, offset) or self.__fix_with_filler(sentence, match, offset) or sentence
 
     def correct(self, sentence):
         matches = re.finditer(self.split_reg, sentence)
@@ -79,3 +94,7 @@ vocabulary = set()
 with open("lexicon.txt", 'r', encoding='utf-8') as f:
     for line in f:
         vocabulary.add(line.strip())
+
+filler = Filler()
+
+splc = SplitCorrector(vocabulary, filler)
--- Metodi_correzione/bert/utils.py
+++ Metodi_correzione/bert/utils.py
@@ -14,3 +14,25 @@ def cleanOutput(output: str):
     output = re.sub(r'\s\s+', " ", output)
     output = output.strip()
     return output
+
+
+def string_subtract(big_str: str, sml_str: str):
+    """
+        Parameters
+        ----------
+        big_str : str
+            Big string to be subtracted
+        sml_string : str
+            Small string that we want to know if it's at the beginning or at the end of the big_str
+
+        Returns
+        ----------
+        If the sml_str is found at the beginning or at the end of the big_str, a tuple with the fragmentation is return, otherwise None.
+        """
+    # Check on the left
+    if (big_str[:len(sml_str)] == sml_str):
+        return sml_str, big_str[len(sml_str):]
+    # Check on the right
+    if (big_str[len(big_str)-len(sml_str):] == sml_str):
+        return big_str[:len(big_str)-len(sml_str)], sml_str
+    return None, None
