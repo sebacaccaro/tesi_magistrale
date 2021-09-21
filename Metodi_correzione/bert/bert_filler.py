79c98a6aba7f4939d7009306e1dc63e004771274
sebacaccaro <sebastianocaccaro@gmail.com>
2021-09-21 02:53:41 +0200

    fine_tuned_model_support

--- Metodi_correzione/bert/bert_filler.py
+++ Metodi_correzione/bert/bert_filler.py
@@ -1,14 +1,36 @@
-from transformers import pipeline
+from transformers import pipeline, AutoModel, AutoTokenizer, BertTokenizer
 from Levenshtein import distance
 
 
-class Filler:
+class Filler_FromWeb:
     MASK_STR = "[MASK]"
     FILL_STR = "exexerrorexex"
 
     def __init__(self) -> None:
         self.unmasker = pipeline(
             'fill-mask', model='dbmdz/bert-base-italian-xxl-cased', top_k=20)
+
+    def guess(self, sentence: str, masked_word: str) -> str:
+        # This is done in order to allow to tokenize/detokenize without
+        # problem from brackets
+        sentence = sentence.replace(self.FILL_STR, self.MASK_STR)
+        results = self.unmasker(sentence)
+
+        # Selecting the closer option among the results
+        results = sorted(results, key=lambda x: distance(
+            x["token_str"], masked_word))
+
+        return results[0]["token_str"]
+
+
+class Filler:
+    MASK_STR = "[MASK]"
+    FILL_STR = "exexerrorexex"
+    MODEL_PATH = 'fine_tuning/fine_tuned/'
+
+    def __init__(self) -> None:
+        self.unmasker = pipeline(
+            'fill-mask', model=self.MODEL_PATH, top_k=20)
 
     def guess(self, sentence: str, masked_word: str) -> str:
         # This is done in order to allow to tokenize/detokenize without
